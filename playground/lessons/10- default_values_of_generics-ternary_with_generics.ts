c0b632ddd02f1ee952777b14a8a8c3b921b30b20
DreamEcho100 <maze6572198@gmail.com>
2021-11-18 16:33:52 +0200

    - Adding In playground/lessons: Add '10- default_values_of_generics-ternary_with_generics.ts'.

---
+++ playground/lessons/10- default_values_of_generics-ternary_with_generics.ts
@@ -0,0 +1,24 @@
+interface Person {
+  name: string
+}
+
+interface Student extends Person {
+  age: number
+}
+
+type StudentInfo<T extends any = Student> = T extends Student ? {
+  data: T
+  grades: number[]
+} : string
+
+const logStudentInfo = (info: StudentInfo<Student>) => console.log(info);
+
+const studentInfo = {
+  data: {
+    name: "MAzen",
+    age: 21
+  },
+  grades: [8, 7, 9, 7]
+}
+
+logStudentInfo(studentInfo);
